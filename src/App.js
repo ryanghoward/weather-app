4a0b5acbe49053d7762ed01ecc229783b93c724d
Ryan Howard <ryanghoward@protonmail.com>
2022-07-29 14:23:12 -0700

    Added Unicons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <h1 className='text-2xl font-bold text-red-500'>Hello</h1>
+      <UilReact size={50} />
     </div>
   );
 }
