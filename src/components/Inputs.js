cc20c7554caa9e33dfacf8034144d64a92f5c41b
Ryan Howard <ryanghoward@protonmail.com>
2022-07-29 15:28:36 -0700

    Search bar added

---
+++ src/components/Inputs.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { UilSearchAlt, UilMapMarker } from "@iconscout/react-unicons";
+
+function Inputs() {
+  return (
+    <div className='flex flex-row justify-center my-6'>
+      <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
+        <input
+          type='text'
+          className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'
+          placeholder='Search cities...'
+        />
+        <UilSearchAlt
+          size={25}
+          className='text-white cursor-pointer transition ease-out hover:scale-125'
+        />
+        <UilMapMarker
+          size={25}
+          className='text-white cursor-pointer transition ease-out hover:scale-125'
+        />
+      </div>
+      <div className='flex flex-row w-1/4 items-center justify-center'>
+        <button name='imperial' className='text-xl text-white font-light'>
+          °F
+        </button>
+        <p className='text-xl text-white mx-1'>|</p>
+        <button name='metric' className='text-xl text-white font-light'>
+          °C
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Inputs;
--- src/components/TopButtons.js
+++ src/components/TopButtons.js
@@ -27,7 +27,9 @@ function TopButtons() {
   return (
     <div className='flex items-center justify-around my-6'>
       {cities.map((city) => (
-        <button className='text-white text-lg font-medium'>{city.title}</button>
+        <button key={city.id} className='text-white text-lg font-medium'>
+          {city.title}
+        </button>
       ))}
     </div>
   );
