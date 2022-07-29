d5e943d984b56fa948a8da6075453df115d3c13a
Ryan Howard <ryanghoward@protonmail.com>
2022-07-29 16:42:23 -0700

    Hard coded even more data

--- src/components/TempAndDetails.js
+++ src/components/TempAndDetails.js
@@ -40,6 +40,27 @@ function TempAndDetails() {
           </div>
         </div>
       </div>
+      <div className='flex flex-row items-center justify-center space-x-2 text-white text-sm py-3'>
+        <UilSun />
+        <p className='font-light'>
+          Rise: <span className='font-medium ml-1'>6:90 AM</span>
+        </p>
+        <p className='font-light'>|</p>
+        <UilSunset />
+        <p className='font-light'>
+          Set: <span className='font-medium ml-1'>6:90 PM</span>
+        </p>
+        <p className='font-light'>|</p>
+        <UilArrowUp />
+        <p className='font-light'>
+          High: <span className='font-medium ml-1'>69°</span>
+        </p>
+        <p className='font-light'>|</p>
+        <UilArrowDown />
+        <p className='font-light'>
+          Low: <span className='font-medium ml-1'>69°</span>
+        </p>
+      </div>
     </div>
   );
 }
