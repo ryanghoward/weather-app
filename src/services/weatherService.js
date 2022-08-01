0f72ac4550c70b27175bcf47cf16201c6bdd657b
Ryan Howard <ryanghoward@protonmail.com>
2022-08-01 11:16:26 -0700

    API

---
+++ src/services/weatherService.js
@@ -0,0 +1,52 @@
+const API_KEY = "aff8915add1c5e1fe72041db0c0cd1f4";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  // console.log(url);
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  return formattedCurrentWeather;
+};
+
+export default getFormattedWeatherData;
