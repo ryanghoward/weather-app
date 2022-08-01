5c268e7d7032184f192817fd77e4910ec79c3ab5
Ryan Howard <ryanghoward@protonmail.com>
2022-08-01 12:40:43 -0700

    Dynamic data passed in

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import "./App.css";
-import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
+import { useState, useEffect } from "react";
 
 function App() {
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "london" });
-    console.log(data);
-  };
+  const [query, setQuery] = useState({ q: "berlin" });
+  const [units, setUnits] = useState("imperial");
+  const [weather, setWeather] = useState(null);
 
-  fetchWeather();
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
 
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400'>
       <TopButtons />
       <Inputs />
-      <TimeAndLocation />
-      <TempAndDetails />
-      <Forecast title='Hourly Forecast' />
-      <Forecast title='Daily Forecast' />
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TempAndDetails weather={weather} />
+          <Forecast title='Hourly Forecast' />
+          <Forecast title='Daily Forecast' />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/TimeAndLocation.js
+++ src/components/TimeAndLocation.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { formatToLocalTime } from "../services/weatherService";
 
-function TimeAndLocation() {
+function TimeAndLocation({ weather: { dt, timezone, name, country } }) {
   return (
     <div>
       <div className='flex items-center justify-center my-6'>
         <p className='text-white text-xl font-extralight'>
-          Thursday, 6 September 1969 | Local Time: 6:90 PM
+          {formatToLocalTime(dt, timezone)}
         </p>
       </div>
       <div className='flex items-center justify-center my-3'>
-        <p className='text-white text-3xl font-medium'>Los Angles, USA</p>
+        <p className='text-white text-3xl font-medium'>{`${name}, ${country}`}</p>
       </div>
     </div>
   );
